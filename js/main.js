3246cd5bf778f765decf4817bee0715d1e3d1a61
JosefaHerrera <josefa@linuxero.cl>
2017-06-15 15:32:14 -0400

    haciendo la pagina nº 2

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
-main.js
+// JS Nat para validar el formulario de contacto
+var botonEnviar = document.getElementById('enviar');
+botonEnviar.addEventListener('click', validarContact);
+
+function validarContact() {
+    // Obtengo los valores del formulario
+    var name = document.getElementById('name');
+    var pwd = document.getElementById('password');
+
+
+    // Si los value vienen vacios envio mensaje
+    if (name.value === '') {
+        name.setAttribute('placeholder', 'llene este campo por favor');
+    }
+    if (pwd.value === '') {
+        pwd.setAttribute('placeholder', 'llene este campo por favor');
+    }
+    if (name.value !== '' && pwd.value ) {
+        botonEnviar.setAttribute('type', 'submit');
+    }
+}
